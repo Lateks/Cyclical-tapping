06849f5d08be5d838fcb8f743dd3f7fb0a313927
Laura Leppänen <laura.leppanen@iki.fi>
2012-01-12 21:44:43 +0200

    Added main.

--- circulartapping.py
+++ circulartapping.py
@@ -1,36 +1,7 @@
 import sys, pygame
 from math import floor, sin, cos, radians
 
-def initialize_ball_positions(screen_width, ball_radius, number_of_balls):
-    positions = list()
-    first_pos = int(floor(screen_width/2)), ball_radius
-    positions.append(first_pos)
-
-    circle_radius = get_circle_radius(screen_width, ball_radius)
-    circle_mid = ball_radius + circle_radius
-    angle_difference = radians(int(floor(360 / num_balls)))
-    angle = radians(0)
-    for i in range(1, number_of_balls):
-        angle += angle_difference
-        positions.append(next_position(angle, circle_radius, circle_mid))
-
-    return positions
-
-def next_position(angle, circle_radius, circle_midpoint):
-    adjacent = adjacent_edge(angle, circle_radius)
-    opposite = opposite_edge(angle, circle_radius)
-    return circle_midpoint + adjacent, circle_midpoint - opposite
-
-def adjacent_edge(angle, hypotenuse):
-    return int(floor(sin(angle) * hypotenuse))
-
-def opposite_edge(angle, hypotenuse):
-    return int(floor(cos(angle) * hypotenuse))
-
-def get_circle_radius(screen_width, ball_radius):
-    return int(floor((screen_width - 2 * ball_radius) / 2))
-
-if __name__ == '__main__':
+def main():
     width = height = 640 # TODO: read these variables from a settings file
     ball_radius = 50     # (should be given as a parameter?)
     num_balls = 9
@@ -56,3 +27,35 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 sys.exit(0)
 
+
+def initialize_ball_positions(screen_width, ball_radius, number_of_balls):
+    positions = list()
+    first_pos = int(floor(screen_width/2)), ball_radius
+    positions.append(first_pos)
+
+    circle_radius = get_circle_radius(screen_width, ball_radius)
+    circle_mid = ball_radius + circle_radius
+    angle_difference = radians(int(floor(360 / number_of_balls)))
+    angle = radians(0)
+    for i in range(1, number_of_balls):
+        angle += angle_difference
+        positions.append(next_position(angle, circle_radius, circle_mid))
+
+    return positions
+
+def next_position(angle, circle_radius, circle_midpoint):
+    adjacent = adjacent_edge(angle, circle_radius)
+    opposite = opposite_edge(angle, circle_radius)
+    return circle_midpoint + adjacent, circle_midpoint - opposite
+
+def adjacent_edge(angle, hypotenuse):
+    return int(floor(sin(angle) * hypotenuse))
+
+def opposite_edge(angle, hypotenuse):
+    return int(floor(cos(angle) * hypotenuse))
+
+def get_circle_radius(screen_width, ball_radius):
+    return int(floor((screen_width - 2 * ball_radius) / 2))
+
+if __name__ == '__main__':
+    main()

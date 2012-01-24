c0b353c554262d9b0a1b48dbf03f164c246f7d3f
Laura Leppänen <laura.leppanen@iki.fi>
2012-01-24 13:02:58 +0200

    Refactoring: a new module for parameter parsing.

--- circulartapping.py
+++ circulartapping.py
@@ -3,6 +3,7 @@ from math import floor
 from geometry import CircleCircumference
 from screen import PygameDisplayWindow
 from logmouse import MouseLogger
+from parameter_parser import Parameters
 
 def main():
     params = Parameters()
@@ -27,66 +28,6 @@ def main():
     mouse_fps = 100
     runner = ProgRunner(screen, mouselog, mouse_fps, 3 * num_plates)
     runner.run()
-
-class Parameters(object):
-    width, height = 640, 640
-    plate_radius = 50
-    num_plates = 9
-    plate_color = 244, 238, 224
-
-    def __init__(self):
-        try:
-            self.config_file = open("config", "r")
-            self.__read_parameters()
-            self.config_file.close()
-        except IOError:
-            print "Could not open the config file. Using defaults."
-        except ParameterError as value:
-            print "Invalid config file or parameters:\n" + \
-                  "%s\nUsing defaults." % value
-            self.config_file.close()
-
-    def __read_parameters(self):
-        parameters = list()
-        for line in self.config_file:
-            if self.__line_is_not_comment(line):
-                tokens = self.__tokenize_line(line)
-                parameters += tokens
-        self.__unlist_parameters(parameters)
-
-    def __line_is_not_comment(self, line):
-        return not line[0] == '#'
-
-    def __tokenize_line(self, line):
-        tokens = list()
-        line_params = line.split(' ')
-        for token in line_params:
-            token = self.__param_to_int(token)
-            tokens.append(token)
-        return tokens
-
-    def __param_to_int(self, string):
-        try:
-            integer = int(string)
-            if integer < 0:
-                raise ParameterError("Negative parameters given. Should be positive.")
-            return integer
-        except ValueError:
-            raise ParameterError("Non-numeric parameters given.")
-
-    def __unlist_parameters(self, parameters):
-        if len(parameters) < 7:
-            raise ParameterError("Parameters missing.")
-        self.width, self.height = parameters[0], parameters[1]
-        self.plate_radius = parameters[2]
-        self.num_plates = parameters[3]
-        self.plate_color = parameters[4], parameters[5], parameters[6]
-
-class ParameterError(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
 
 class ProgRunner(object):
     def __init__(self, screen, mouselogger, log_fps, trial_length_in_clicks):
